a65e59f76887fd11e4ce53c325c56faeb8cba494
Arbaj Shekh <arbaj.shekh@madiba.com>
2026-08-14 16:08:28 +0530

    deployement config with some changes

---
+++ dist/Component.js
@@ -0,0 +1,2 @@
+sap.ui.define(["sap/ui/core/UIComponent","sap/ui/Device","sap/ui/model/json/JSONModel"],function(e,i,t){"use strict";return e.extend("ZFI_INTERCO.Component",{metadata:{manifest:"json"},init:function(){e.prototype.init.apply(this,arguments);var n=new t(i);n.setDefaultBindingMode("OneWay");this.setModel(n,"device");this.getRouter().initialize()}})});
+//# sourceMappingURL=Component.js.map
